ebbfbac4967410a76d31f8b251fb7bf9b87818c0
Ruy Diaz <diazruy@gmail.com>
2013-08-24 22:52:50 -0700

    Solution for exercise 02

--- app/assets/javascripts/geocoder.js
+++ app/assets/javascripts/geocoder.js
@@ -1,2 +1,22 @@
 var Geocoder = function(){
+  var geocode = function(address, callback){
+    url = "http://maps.googleapis.com/maps/api/geocode/json";
+    params = {
+      sensor: false,
+      address: address
+    };
+
+    $.getJSON(url, params, function(data, status, xhr){
+      firstResult = data.results[0];
+      if(typeof firstResult !== 'undefined') {
+        callback(firstResult.formatted_address);
+      } else {
+        callback(null);
+      }
+    });
+  };
+
+  return {
+    geocode: geocode
+  };
 };
--- spec/javascripts/geocoder_spec.js
+++ spec/javascripts/geocoder_spec.js
@@ -2,34 +2,32 @@
 describe('Geocoder', function(){
   var geocoder;
 
-  // Google's Geocoding API lives at http://maps.googleapis.com/maps/api/geocode/json
-  // It takes the following GET parameters:
-  // - sensor: (boolean) Indicates whether or not the geocoding request comes
-  //           from a device with a location sensor
-  // - address: (String) The address you want to geocode
-  //
-  // The response is a JSON object like the following:
-  // {results: [
-  //   {formatted_addres: '123 12th St', ...},
-  //   {formatted_address: '123 12th Ave', ...}
-  // ]}
-  //
-  // Create a Geocoder class with a #geocode method which will take an address
-  // to search for and a callback to execute with the first available result, or null
-  // if none is found
-  //
-  // Example usage:
-  // var geocoder = new Geocoder();
-  // geocoder.geocode(address, function(address){ console.log(address);});
-  //
-  // Use $.getJSON() to get the content. In the spec, stub out this call
-  // to prevent hitting an external resource
+  beforeEach(function(){
+    geocoder = new Geocoder();
+  });
 
   it("executes the callback with the first matched address as an argument", function(){
-    expect(false).toBeTruthy();
+    var address = "Waterfront Station, Vancouver";
+    var callback = jasmine.createSpy();
+
+    spyOn($, 'getJSON').andCallFake(function(url, params, jsonCallback){
+      jsonCallback({results: [{formatted_address: 'Waterfront Skytrain, Vancouver, BC V6B, Canada'}]});
+    });
+
+    geocoder.geocode(address, callback);
+    expect(callback).toHaveBeenCalled();
+    expect(callback).toHaveBeenCalledWith('Waterfront Skytrain, Vancouver, BC V6B, Canada');
   });
 
   it('returns null when no results are found', function(){
-    expect(false).toBeTruthy();
+    var address = "asfasdasdf";
+    var callback = jasmine.createSpy();
+
+    spyOn($, 'getJSON').andCallFake(function(url, params, jsonCallback){
+      jsonCallback({results: []});
+    });
+
+    geocoder.geocode(address, callback);
+    expect(callback).toHaveBeenCalledWith(null);
   });
 });
